170f184f3196fbef2bfc2d83c6662b937c93a61e
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-11-14 20:47:15 -0700

    worked on components and buttons

---
+++ dreamers-app/src/Components/Log.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Log extends Component {
+  render (){
+    return (<p className = "App-log"> This is the log component </p>)
+  }
+}
