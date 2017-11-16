d1b1a9b4e0892071abf0ce84370397cd5088793e
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-11-15 18:30:00 -0700

    created log page inputs

--- dreamers-app/src/Components/Analyze.js
+++ dreamers-app/src/Components/Analyze.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class Analyze extends Component {
   render (){
-    return (<p className = "App-analyze"> This is the analyze component </p>)
+    return (<p>hey you</p>)
   }
 }
