04f889ab4aa3e631fcad267e6ea9e10b04c938bf
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-11-21 20:29:50 -0700

    uninstalled burger-menu and fixed naming problems

--- dreamers-app/src/Components/Log.js
+++ dreamers-app/src/Components/Log.js
@@ -1,35 +1,33 @@
 import React, { Component } from 'react';
 import {Form, FormControl, FormGroup, ControlLabel, Button} from 'react-bootstrap';
 
-const logStyle = {
+const logControlStyle = {
   height: '20rem',
   margin: '0 auto',
   width: '80%',
   paddingRight: '0.5rem'
 }
 
-const formInstance = (
-  <Form inline>
-    <FormGroup controlId="formInlineTitle">
-      <ControlLabel>Title</ControlLabel>
-        {' '}
-      <FormControl type="text" placeholder=" " />
-      </FormGroup>
-        {' '}
-    <FormGroup controlId="formInlineDream">
-      <ControlLabel>Dream</ControlLabel>
-        {' '}
-      <FormControl style={logStyle} type="text" placeholder=" " />
-    </FormGroup>
-        {' '}
-    <Button type="submit">
-        Submit & Save
-    </Button>
-  </Form>
-);
-
 export default class Log extends Component {
   render (){
-    return (formInstance)
+    return (
+      <Form inline>
+        <FormGroup controlId="formInlineTitle">
+          <ControlLabel>Title</ControlLabel>
+            {' '}
+          <FormControl type="text" placeholder=" " />
+          </FormGroup>
+            {' '}
+        <FormGroup controlId="formInlineDream">
+          <ControlLabel>Dream</ControlLabel>
+            {' '}
+          <FormControl style={logControlStyle} type="text" placeholder=" " />
+        </FormGroup>
+            {' '}
+        <Button type="submit">
+            Submit & Save
+        </Button>
+      </Form>
+    )
   }
 }
